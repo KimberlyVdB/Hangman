1d17fcf2c6cc40cc440921c515b4e3a00908a79d
SugarcubesLaptop <Kimberly.VandenBroeck@hotmail.com>
2019-05-13 15:56:17 +0200

    rewrote my done function and cleaned up code

--- src/words.js
+++ src/words.js
@@ -1,14 +1,13 @@
 import words from './longwords.json'
-import React from 'react';
 
 export const getRandomWord = () => {
     return words[Math.floor(Math.random() * words.length)];
 };
 
 export const renderWord = (word, guesses) => {
-
+    console.log(guesses)
     const lettersOfWord = word.split("").map(letter => guesses.indexOf(letter) >= 0 ? letter : " _").join(' ');
-    console.log(lettersOfWord);
+    console.log(word, lettersOfWord);
     let answereArray = [];
     for (let i = 0; i < word.length; i++) {
         const letter = word[i]
@@ -21,25 +20,8 @@ export const renderWord = (word, guesses) => {
     return answereArray;
 }
 
-function removeDoubles(letter, index, letters) {
-    return letters.indexOf(letter) === index;
-}
-
 export const done = (word, guesses) => {
-    //    guesses = guesses.filter(removeDoubles);
-    const wordToGuess = word.split('').filter(removeDoubles);
-    const goodGuesses = guesses.filter(function (guess) {
-        return word.includes(guess);
-    })
-    const badGuesses = guesses.filter(function (guess, index, guesses) {
-        return !word.includes(guess)
-    });
-    if (goodGuesses.length === wordToGuess.length) {
-        return <h1>I guess we arent hanging out</h1>
-    } else if (badGuesses.length >= 6) {
-        return <h1>lol you died</h1>
-    }
+    const foo = renderWord(word, guesses).join('')
+    if (word === foo)
+        return true
 }
-    //  return goodGuesses.length === wordToGuess.length || badGuesses.length >= 6;
-
-//compair word with renderWord
