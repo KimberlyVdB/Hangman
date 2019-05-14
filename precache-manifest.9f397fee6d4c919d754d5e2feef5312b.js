f0e6ef93da169135bb58b2be0c8664d537d2ebae
SugarcubesLaptop <Kimberly.VandenBroeck@hotmail.com>
2019-05-14 16:52:42 +0200

    Updates

---
+++ precache-manifest.9f397fee6d4c919d754d5e2feef5312b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c6a8751692267364c399cc83da85f9",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "7d04a4a8fe4f822a70ce",
+    "url": "/Hangman/static/css/main.c7182a50.chunk.css"
+  },
+  {
+    "revision": "bedea4066ca0672b7b81",
+    "url": "/Hangman/static/js/2.83b9a475.chunk.js"
+  },
+  {
+    "revision": "7d04a4a8fe4f822a70ce",
+    "url": "/Hangman/static/js/main.010fb228.chunk.js"
+  },
+  {
+    "revision": "f8832925578ad51d01a6",
+    "url": "/Hangman/static/js/runtime~main.9bccb078.js"
+  }
+]);
